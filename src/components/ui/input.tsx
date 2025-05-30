2aa1b8d8d32902840fd1f003339894d611192dc8
Akiz-Ivanov <ivanovicandreja@gmail.com>
2025-05-30 11:46:27 +0200

    fix: input invalid focus ring

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -10,7 +10,7 @@ function Input({ className, type, ...props }: React.ComponentProps<"input">) {
       className={cn(
         "file:text-foreground selection:bg-accent-blue selection:text-primary-foreground border-input flex w-full min-w-0 border bg-transparent shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50",
         "focus-visible:border-ring focus-visible:ring-accent-blue focus-visible:ring-[0.8px]",
-        "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-[#ca8382] aria-invalid:border-2 aria-invalid:focus-visible:ring-4",
+        "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-[#ca8382] aria-invalid:border-2 aria-invalid:focus-visible:ring-3",
         className
       )}
       {...props}
